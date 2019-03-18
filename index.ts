b478e9fce71929d78df7e8d78e54e6dde3abf1cb
yunheur <yunheur@gmail.com>
2019-03-18 20:22:12 +0900

    write index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello")
+const name = "yunheur",
+age = 28,
+gender = "male";
+
+// gender는 선택
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHi(name, age)
+
+export {};
