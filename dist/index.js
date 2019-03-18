c03757e766013e778cc3db61a4b7cf3c1fe6b82a
yunheur <yunheur@gmail.com>
2019-03-18 20:49:28 +0900

    classes on typescript - part 1

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "yunheur",
-    age: 28,
-    gender: "male"
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const yunheur = new Human("yunheur", 28, "male");
 const sayHi = (person) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
-console.log(sayHi(person));
+console.log(sayHi(yunheur));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-  name: string,
-  age: number,
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "yunheur",
-  age: 28,
-  gender: "male"
-}
+const yunheur = new Human("yunheur", 28, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(yunheur));
 
 export {};
